a1c93a5f4158d773867eeef4b8aaf5d21cc7007e
APPLE <APPLE@APPLEs-MacBook-Air.local>
2018-12-24 12:29:02 +0330

    Project started

---
+++ App.js
@@ -0,0 +1,52 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View , ImageBackground, Image} from 'react-native';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <Image source={require('./App/Img/logo.png')} > 
+      </Image>
+     /* <ImageBackground style={{width: 60, height: 50}} source={require('./App/Img/logo.png')} style={styles.container}>
+        <Text style={styles.welcome}> Welcome to Iranina Legal Medicine Registry</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>{instructions}</Text>
+      </ImageBackground> */
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
